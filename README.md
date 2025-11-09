99f1f5d5e62357d69aa7245b1fe35d2449813dcc
Aman Yadav <amanyadav665078@gmail.com>
2025-11-09 15:46:39 +0530

    Final Commit

--- README.md
+++ README.md
@@ -2,16 +2,17 @@ This is a Simple research agent with different Parts running toghether to comple
 
 
 #FIRST GRAPH
-
+This is the Research team with different nodes, which helps to do research and scrape the web pages.
 ![Alt text](home/aman/Downloads/first_graph.png)
 
-
 #SECOND GRAPH
+Helps to write the docs, edit and update the docs based on the given input fromt the supervisor taken from research team
 ![Alt text](home/aman/Downloads/lang.png)
 
-
-
-
 #FINAL GRAPH
-
+The supervisor manages the whole worksflow by combining all the nodes in to two different teams which makes sense.
 ![Alt text](home/aman/Downloads/new_lang.png)
+
+
+This was fairly simple project, I will tryin to build more useful and complex project than this .
+Thank you ....
--- src/doc_team.py
+++ src/doc_team.py
@@ -105,3 +105,57 @@ for s in paper_writing_graph.stream(
 ):
     print(s)
     print("-------")
+
+
+
+
+# Adding Layers to control the whole process as a team not a single node, processing it as a single node might be diffcutl so we can combine the
+# in team that would be helpful as well as quite easiy to handle
+from .research_team import research_graph
+from langchain_core.messages import BaseMessage
+
+llm = ChatOllama(base_url="http://localhost:11434", model="gpt-oss:120b-cloud")
+
+teams_supervisor_node = make_supervisor_node(llm, ["research_team", "writing_team"])
+
+
+
+def call_research_team(state: State) -> Command[Literal["supervisor"]]:
+    response = research_graph.invoke({"messages": state["messages"][-1]})
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(
+                    content=response["messages"][-1].content, name="research_team"
+                )
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+def call_paper_writing_team(state: State) -> Command[Literal["supervisor"]]:
+    response = paper_writing_graph.invoke({"messages": state["messages"][-1]})
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(
+                    content=response["messages"][-1].content, name="writing_team"
+                )
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+# Define the graph.
+super_builder = StateGraph(State)
+super_builder.add_node("supervisor", teams_supervisor_node)
+super_builder.add_node("research_team", call_research_team)
+super_builder.add_node("writing_team", call_paper_writing_team)
+
+super_builder.add_edge(START, "supervisor")
+super_graph = super_builder.compile()
+
+
+display(Image(super_graph.get_graph().draw_mermaid_png()))
